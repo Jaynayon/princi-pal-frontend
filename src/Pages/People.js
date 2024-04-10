aafd6c7ac5fda199b151d08a5bf4660caf1b5518
kolay404 <angelnicole.b11@gmail.com>
2024-04-10 16:57:36 +0800

    changes

--- src/Pages/People.js
+++ src/Pages/People.js
@@ -2,45 +2,41 @@ import React from 'react';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button'; 
+import './People.css';
 
 function People(props) {
     return (
-        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-            <Grid container spacing={3}>
-                {/* Chart */}
-                <Grid item xs={12} md={8} lg={9}>
-                    <Paper
-                        sx={{
-                            p: 2,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            height: 240,
-                        }}
-                    >
-
-                    </Paper>
+        <Box
+            component="form"
+            noValidate
+            autoComplete="off"
+        >
+            <Grid container spacing={0} alignItems="center">
+                <Grid>
+                    <TextField id="search" label="Search by name or email" variant="outlined" className="searchTextField" />
                 </Grid>
-                {/* Recent Deposits */}
-                <Grid item xs={12} md={4} lg={3}>
-                    <Paper
-                        sx={{
-                            p: 2,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            height: 240,
-                        }}
-                    >
-
-                    </Paper>
+                <Grid>
+                    <TextField id="invite" label="Invite by email" variant="outlined" className="inviteTextField" />
                 </Grid>
-                {/* Recent Orders */}
-                <Grid item xs={12}>
-                    <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-
-                    </Paper>
+                <Grid>
+                    <Button variant="contained" className="inviteButton">Invite</Button>
                 </Grid>
             </Grid>
-        </Container>
+            <Container maxWidth="lg" sx={{ mt: 3, mb: 3 }}>
+                <Grid container spacing={3}>
+                    <Grid item xs={12} md={8} lg={9}>
+                        <Paper className='paperContainer'>
+                            {/* Content */}
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={12} md={4} lg={3}>
+                    </Grid>
+                </Grid>
+            </Container>
+        </Box>
     );
 }
 
