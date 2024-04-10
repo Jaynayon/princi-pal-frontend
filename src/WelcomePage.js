6285d9c45181c8143057309adeb955cb1549779f
janickaalgonas <janickamarie.algonas@cit.edugit config --global user.name>
2024-04-10 13:51:38 +0800

    Modify using MUI Material

--- src/WelcomePage.js
+++ src/WelcomePage.js
@@ -1,47 +1,38 @@
+import React from "react";
 import Layout from "./Components/Welcome/Layout";
+import { Typography, Container, Box } from "@mui/material";
 
 const WelcomePage = () => {
   return (
-    <div
-      style={{
-        width: "100%",
+    <Container
+      sx={{
         position: "relative",
         backgroundColor: "#fff",
-        height: "1024px",
-        overflow: "hidden",
+        minHeight: "100vh",
         textAlign: "left",
         fontSize: "24px",
         color: "#4a99d3",
-        fontFamily: "Mulish", // Set font family to Mulish
+        fontFamily: "Mulish",
+        margin: 0,
       }}
     >
-      <div
-        style={{
+      <Box
+        sx={{
           position: "absolute",
-          top: "0px",
-          left: "0px",
+          top: "1024px",
+          left: "1440px",
+          background:
+            "linear-gradient(1.02deg, #4a99d3 7.81%, rgba(74, 153, 211, 0)), #fff",
+          boxShadow: "0px 4px 16px rgba(75, 0, 129, 0.26)",
           width: "1440px",
           height: "1024px",
+          transform: "rotate(-180deg)",
+          transformOrigin: "0 0",
+          opacity: "0.2",
         }}
-      >
-        <div
-          style={{
-            position: "absolute",
-            top: "1024px",
-            left: "1440px",
-            background:
-              "linear-gradient(1.02deg, #4a99d3 7.81%, rgba(74, 153, 211, 0)), #fff",
-            boxShadow: "0px 4px 16px rgba(75, 0, 129, 0.26)",
-            width: "1440px",
-            height: "1024px",
-            transform: " rotate(-180deg)",
-            transformOrigin: "0 0",
-            opacity: "0.2",
-          }}
-        />
-      </div>
-      <div
-        style={{
+      />
+      <Box
+        sx={{
           position: "absolute",
           top: "300px", // Adjusted top position for the description text
           left: "680px",
@@ -49,50 +40,17 @@ const WelcomePage = () => {
           height: "360px",
         }}
       >
-        <div
-          style={{
-            position: "absolute",
-            top: "0px",
-            left: "0px",
-            fontSize: "56px",
-            fontWeight: "500",
-            display: "inline-block",
-            width: "689px",
-            height: "262px",
-          }}
-        >
+        <Typography variant="h3" fontWeight="500" sx={{ marginBottom: "20px" }}>
           PrinciPal: Streamlined Public School Document Management System
-        </div>
-        <div
-          style={{
-            position: "absolute",
-            top: "282px", // Adjusted top position for the description text
-            left: "0px",
-            lineHeight: "140.63%",
-            display: "inline-block",
-            width: "670px",
-            height: "135px",
-          }}
-        >
+        </Typography>
+        <Typography variant="body1" sx={{ lineHeight: "140.63%" }}>
           The pinnacle of innovation in public school document management. Step
           into a realm where efficiency, organization, and productivity converge
           to redefine how educational institutions handle their vital paperwork.
-        </div>
-      </div>
+        </Typography>
+      </Box>
       <Layout />
-      <img
-        style={{
-          position: "absolute",
-          top: "90px",
-          left: "149px",
-          width: "243px",
-          height: "185px",
-          objectFit: "cover",
-        }}
-        alt=""
-        src="/logoremovebgpreview-1@2x.png"
-      />
-    </div>
+    </Container>
   );
 };
 
