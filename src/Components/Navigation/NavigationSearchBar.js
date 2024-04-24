8539f0c39fb5ceb2c26f9d8661a68f79fff6aa8a
Allen Tiempo <jatiempo89@gmail.com>
2024-04-24 20:58:14 +0800

    added some changes in the module 7 dialog box

--- src/Components/Navigation/NavigationSearchBar.js
+++ src/Components/Navigation/NavigationSearchBar.js
@@ -4,26 +4,64 @@ import InputBase from "@mui/material/InputBase";
 import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 
+import Dialog from "@mui/material/Dialog";
+import Button from "@mui/material/Button";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Avatar from "@mui/material/Avatar";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+
+const SCHOOLS = [
+  "University of San Carlos",
+  "University of the Philippines Cebu",
+  "Cebu Institute of Technology - University",
+  "University of Cebu",
+  "Southwestern University",
+  "Cebu Doctors' University",
+  "University of San Jose - Recoletos",
+  "Cebu Normal University",
+  "University of Southern Philippines Foundation",
+  "Asian College of Technology",
+];
+
 const NavigationSearchBar = () => {
   const [query, setQuery] = useState("");
-  const [schools, setSchools] = useState([
-    "University of San Carlos",
-    "University of the Philippines Cebu",
-    "Cebu Institute of Technology - University",
-    "University of Cebu",
-    "Southwestern University",
-    "Cebu Doctors' University",
-    "University of San Jose - Recoletos",
-    "Cebu Normal University",
-    "University of Southern Philippines Foundation",
-    "Asian College of Technology",
-  ]);
+  const [open, setOpen] = useState(false);
+  const [selectedSchool, setSelectedSchool] = useState("");
+  const [openApplicationInbox, setOpenApplicationInbox] = useState(false);
+  const [select, setSelect] = useState("");
+
+  const handleChange = (event) => {
+    setSelect(event.target.value);
+  };
+
+  const handleClickOpenApplicationInbox = () => {
+    setOpenApplicationInbox(true);
+  };
+
+  const handleClickCloseApplicationInbox = () => {
+    setOpenApplicationInbox(false);
+  };
+
+  const handleClickOpen = (school) => {
+    setSelectedSchool(school);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const handleInputChange = (event) => {
     setQuery(event.target.value);
   };
 
-  const filteredSchools = schools.filter((school) =>
+  const filteredSchools = SCHOOLS.filter((school) =>
     school.toLowerCase().includes(query.toLowerCase())
   );
   return (
@@ -67,20 +105,130 @@ const NavigationSearchBar = () => {
               padding: "8px 16px",
               borderBottom: "1px solid #ccc",
               textDecoration: "underline",
+              cursor: "pointer",
             }}
+            onClick={handleClickOpenApplicationInbox}
           >
             Application Inbox
           </li>
+          <Dialog
+            open={openApplicationInbox}
+            onClose={handleClickCloseApplicationInbox}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">
+              {"Application Inbox"}
+            </DialogTitle>
+            <DialogContent
+              sx={{
+                display: "flex",
+                justifyContent: "spaceBetween",
+                alignItems: "center",
+                gap: "1rem",
+              }}
+            >
+              <Avatar>C</Avatar>
+              <DialogContentText id="alert-dialog-description">
+                <span style={{ fontWeight: "bold" }}>
+                  Cebu Institute of Technology - University
+                </span>
+                <br />
+                Your application is currently under review.
+              </DialogContentText>
+              <DialogActions>
+                <Button onClick={handleClickCloseApplicationInbox} autoFocus>
+                  Cancel
+                </Button>
+              </DialogActions>
+            </DialogContent>
+
+            <DialogContent
+              sx={{
+                display: "flex",
+                justifyContent: "spaceBetween",
+                alignItems: "center",
+                gap: "1rem",
+              }}
+            >
+              <Avatar>C</Avatar>
+              <DialogContentText id="alert-dialog-description">
+                <span style={{ fontWeight: "bold" }}>
+                  Cebu Institute of Medicine
+                </span>
+                <br />
+                Your application is currently under review.
+              </DialogContentText>
+              <DialogActions>
+                <Button onClick={handleClickCloseApplicationInbox} autoFocus>
+                  Cancel
+                </Button>
+              </DialogActions>
+            </DialogContent>
+          </Dialog>
           {filteredSchools.map((school, index) => (
             <li
               key={index}
-              style={{ padding: "8px 16px", borderBottom: "1px solid #ccc" }}
+              style={{
+                padding: "8px 16px",
+                borderBottom: "1px solid #ccc",
+                cursor: "pointer",
+                display: "flex",
+                alignItems: "center",
+                gap: "1rem",
+              }}
+              onClick={() => handleClickOpen(school)}
             >
+              <Avatar>C</Avatar>
               {school}
             </li>
           ))}
         </ul>
       )}
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Selected School"}</DialogTitle>
+        <DialogContent
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "start",
+            gap: "1rem",
+          }}
+        >
+          <Avatar>C</Avatar>
+          <DialogContentText id="alert-dialog-description">
+            <span style={{ fontWeight: "bold" }}>{selectedSchool}</span>
+            <br />
+            Provide information to request access to this organization.
+          </DialogContentText>
+          <FormControl sx={{ m: 1, minWidth: 80 }} size="small">
+            <InputLabel id="demo-simple-select-autowidth-label">
+              ADAS
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-autowidth-label"
+              id="demo-simple-select-autowidth"
+              value={select}
+              onChange={handleChange}
+              autoWidth
+              label="Select"
+            >
+              <MenuItem value="ADAS">ADAS</MenuItem>
+              <MenuItem value="ADOF">ADOF</MenuItem>
+            </Select>
+          </FormControl>
+          <DialogActions>
+            <Button onClick={handleClose} autoFocus>
+              Apply
+            </Button>
+          </DialogActions>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
