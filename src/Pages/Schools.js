1341ad0da5160688dafb4589741151fbce90bc2e
Jaynayon <jay.nayonjr@cit.edu>
2024-04-30 09:53:27 +0800

    Migrate RecordTable progress to School.js

--- src/Pages/Schools.js
+++ src/Pages/Schools.js
@@ -1,17 +1,87 @@
 import '../App.css'
 import React from 'react';
 import Paper from '@mui/material/Paper';
-import RecordsTable from '../Components/Table/RecordsTable';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import PropTypes from 'prop-types';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import IconButton from "@mui/material/IconButton";
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import { RecordsProvider } from '../Context/RecordsProvider';
 import { DateFilter, FieldsFilter, SearchFilter } from '../Components/Filters/Filters'
+import LRTable from '../Components/Table/LRTable';
 
 //import { useState } from 'react';
 //import { useEffect } from 'react';
 
-//xs, sm, md sizes are important
+function CustomTabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ paddingTop: 1 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+CustomTabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
+function BudgetSummary(props) {
+    const { title, amount, total = false } = props
+    return (
+        <Paper sx={{ minWidth: 150, height: 65, m: 1, backgroundColor: total ? '#0077B6' : undefined }} variant='outlined'>
+            <Box
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    fontWeight: 'bold',
+                    height: '100%',
+                }}
+            >
+                <Typography variant="body2" align="center" sx={{ fontWeight: 'bold', color: total ? '#ffff' : '#9FA2B4' }}>
+                    {title}
+                </Typography>
+                <Typography variant="body2" align="center" sx={{ fontWeight: 'bold', color: total && '#ffff' }}>
+                    Php {amount}
+                </Typography>
+            </Box>
+        </Paper>
+    );
+}
+
 function Schools(props) {
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
     return (
         <Container className="test" maxWidth="lg" sx={{ /*mt: 4,*/ mb: 4 }}>
             <Grid container spacing={2} sx={{ position: 'relative' }}> {/*relative to allow date component to float*/}
@@ -34,7 +104,81 @@ function Schools(props) {
                     </Paper>
                 </Grid>
                 <Grid item xs={12} md={12} lg={12}>
-                    <RecordsTable />
+                    <RecordsProvider>
+                        <Paper sx={styles.container}>
+                            <Grid container>
+                                <Grid item xs={12} md={12} lg={12}>
+                                    <Box sx={{
+                                        overflow: 'auto', //if overflow, hide it
+                                        overflowWrap: "break-word",
+                                    }}>
+                                        <Tabs sx={{ minHeight: '10px' }}
+                                            value={value}
+                                            onChange={handleChange}
+                                            aria-label="basic tabs example">
+                                            <Tab sx={styles.tab} label="LR" {...a11yProps(0)} />
+                                            <Tab sx={styles.tab} label="Item Two" {...a11yProps(1)} />
+                                            <Tab sx={styles.tab} label="Item Three" {...a11yProps(2)} />
+                                        </Tabs>
+                                    </Box>
+                                </Grid>
+                                <Grid item xs={12} md={12} lg={12} sx={{ pt: 2 }}>
+                                    <Box sx={{
+                                        display: 'flex',
+                                        flexDirection: 'row',
+                                        justifyContent: 'space-between'
+                                    }}
+                                    >
+                                        <Grid container>
+                                            <Grid item xs={12} md={8} lg={6}>
+                                                <Box sx={{
+                                                    display: 'flex',
+                                                    flexDirection: 'row',
+                                                    justifyContent: 'space-between',
+                                                    alignItems: 'center',
+                                                    height: "100%",
+                                                    //backgroundColor: 'green'
+                                                }}
+                                                >
+                                                    <IconButton sx={{ alignSelf: "center" }}>
+                                                        <AddBoxIcon sx={{ fontSize: 25, color: '#20A0F0' }} />
+                                                    </IconButton>
+                                                    <Grid container >
+                                                        <Grid item xs={12} md={12} lg={4}>
+                                                            <BudgetSummary total title="Total" amount="9,675.43" />
+                                                        </Grid>
+                                                        <Grid item xs={12} md={12} lg={4}>
+                                                            <BudgetSummary title="Budget this month" amount="18,000.00" />
+                                                        </Grid>
+                                                        <Grid item xs={12} md={12} lg={4}>
+                                                            <BudgetSummary title="Balance" amount="8,324.57" />
+                                                        </Grid>
+                                                    </Grid>
+                                                </Box>
+                                            </Grid>
+                                            <Grid item xs={12} md={4} lg={6}>
+                                                <Box sx={{ width: '100%', height: '100%' }}>
+                                                    Table Header Content2
+                                                </Box>
+                                            </Grid>
+                                        </Grid>
+                                    </Box>
+                                </Grid>
+                                {/*Document Tables*/}
+                                <Grid item xs={12} md={12} lg={12}>
+                                    <CustomTabPanel value={value} index={0}>
+                                        <LRTable />
+                                    </CustomTabPanel>
+                                    <CustomTabPanel value={value} index={1}>
+                                        Item Two
+                                    </CustomTabPanel>
+                                    <CustomTabPanel value={value} index={2}>
+                                        Item Three
+                                    </CustomTabPanel>
+                                </Grid>
+                            </Grid>
+                        </Paper>
+                    </RecordsProvider>
                 </Grid>
             </Grid>
         </Container >
@@ -53,6 +197,18 @@ const styles = {
             width: '650px', //adjust the container
             //position: 'relative'
         }
+    },
+    container: {
+        overflow: 'hidden',
+        padding: "10px",
+        paddingTop: "10px"
+    },
+    tab: {
+        minHeight: '10px',
+        '&.Mui-selected': {
+            color: 'black', // Color of selected tab
+            fontWeight: 'bold', // Font weight of selected tab
+        },
     },
 }
 
