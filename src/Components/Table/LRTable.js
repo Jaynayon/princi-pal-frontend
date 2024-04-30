4b64dfefab8adf2181558af7cbab1204af416b74
Jaynayon <jay.nayonjr@cit.edu>
2024-04-30 10:13:38 +0800

    Remove unused import

--- src/Components/Table/LRTable.js
+++ src/Components/Table/LRTable.js
@@ -8,7 +8,6 @@ import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import RecordsRow from './RecordsRow';
-import TableHeader from './TableHeader';
 import { RecordsProvider } from '../../Context/RecordsProvider'
 
 function LRTable(props) {
