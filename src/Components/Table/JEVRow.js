6b255f4ff41f0e55189b3170cd8d4e36a79d3eaa
Jaynayon <jay.nayonjr@cit.edu>
2024-05-12 20:23:44 +0800

    Remove unnecessary event handlers

--- src/Components/Table/JEVRow.js
+++ src/Components/Table/JEVRow.js
@@ -3,33 +3,22 @@ import { TableCell, TableRow } from "@mui/material";
 import { useSchoolContext } from '../../Context/SchoolProvider';
 import Box from '@mui/material/Box';
 // import { useNavigationContext } from '../../Context/NavigationProvider';
-import Button from '@mui/material/Button';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import MenuItem from '@mui/material/MenuItem';
-import { Menu, TextField } from '@mui/material';
+import { TextField } from '@mui/material';
 import RestService from '../../Services/RestService';
-import IconButton from "@mui/material/IconButton";
-import CancelIcon from '@mui/icons-material/Cancel';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import UacsDateFilter from '../Filters/UacsDateFilter';
 
 function JEVRow(props) {
     const { rows, setRows, page, rowsPerPage } = props;
     const [editingCell, setEditingCell] = useState({ colId: null, rowId: null });
     const [inputValue, setInputValue] = useState('Initial Value');
     const [initialValue, setInitialValue] = useState(''); //only request update if there is changes in initial value
-    const { displayFields, isAdding, currentDocument, lr, fetchDocumentData, setReload, reload, value } = useSchoolContext();
-
-    const [deleteAnchorEl, setDeleteAnchorEl] = useState(null);
-    const [selectedIndex, setSelectedIndex] = useState(null);
-    const [dropdownAnchorEl, setDropdownAnchorEl] = useState(null);
+    const { currentDocument, fetchDocumentData, setReload, reload, value } = useSchoolContext();
 
     useEffect(() => {
         console.log("RecordsRow useEffect")
         // if (isAdding === true && value === 0) { // applies only to LR & RCD tab: value = 0
         //     displayFields(isAdding);
         // }
-    }, [value, isAdding, displayFields]);
+    }, [value]);
 
     const handleCellClick = (colId, rowId, event) => {
         setEditingCell({ colId, rowId });
@@ -39,42 +28,9 @@ function JEVRow(props) {
         console.log('row Id: ' + rowId + " and col Id: " + colId)
     };
 
-    const handleDeleteOpen = (event, index) => {
-        setDeleteAnchorEl(event.currentTarget);
-        setSelectedIndex(index);
-    };
-
-    const handleMenuClose = () => {
-        setDropdownAnchorEl(null);
-        setDeleteAnchorEl(null);
-        setSelectedIndex(null);
-    };
-
-    const handleDelete = async (rowId) => {
-        // Implement delete functionality here
-        console.log("Delete button clicked for row at index:", selectedIndex);
-        console.log("Delete lr id: " + rowId)
-        deleteLrByid(rowId);
-        handleMenuClose();
-    };
-
-    const deleteLrByid = async (rowId) => {
+    const updateJevById = async (colId, rowId, value) => {
         try {
-            const response = await RestService.deleteLrById(rowId);
-            if (response) {
-                console.log(`LR with id: ${rowId} is deleted`);
-            } else {
-                console.log("LR not deleted");
-            }
-            fetchDocumentData();
-        } catch (error) {
-            console.error('Error fetching document:', error);
-        }
-    };
-
-    const updateLrById = async (colId, rowId, value) => {
-        try {
-            const response = await RestService.updateLrById(colId, rowId, value);
+            const response = await RestService.updateJevById(colId, rowId, value);
             if (response) {
                 console.log(`LR with id: ${rowId} is updated`);
             } else {
@@ -84,46 +40,6 @@ function JEVRow(props) {
         } catch (error) {
             console.error('Error fetching document:', error);
         }
-    }
-
-    const createLrByDocumentId = async (doc_id, obj) => {
-        try {
-            const response = await RestService.createLrByDocId(doc_id, obj);
-            if (response) {
-                console.log(`LR is created`);
-            } else {
-                console.log("LR not created");
-            }
-            fetchDocumentData();
-        } catch (error) {
-            console.error('Error fetching document:', error);
-        }
-    }
-
-    const handleAmountType = (value) => {
-        if (value === true) {
-            return (<span>"Credit"</span>);
-        } else {
-            return (<span>"Debit"</span>);
-        }
-    }
-
-    //If lr length is greater than one; reload to fetch documents and lr createLrByDocId
-    //else, set lr to empty
-    const handleNewRecordCancel = () => {
-        console.log("cancel");
-        if (lr.length > 1) {
-            fetchDocumentData();
-        } else {
-            setReload(!reload); //just to reload school.js to fetch lr data
-        }
-    }
-
-    //Find the index of the lr row where id == 3 and push that value to db
-    const handleNewRecordAccept = (rowId) => {
-        console.log("accept");
-        const rowIndex = rows.findIndex(row => row.id === rowId);
-        createLrByDocumentId(currentDocument.id, rows[rowIndex]);
     }
 
     const handleInputChange = (colId, rowId, event) => {
@@ -148,14 +64,11 @@ function JEVRow(props) {
     const handleInputBlur = (colId, rowId) => {
         setEditingCell(null);
         // Perform any action when input is blurred (e.g., save the value)
-        // Only applies if it's not the new row
-        if (rowId !== 3) {
-            if (inputValue !== initialValue) {
-                console.log(`Wow there is changes in col: ${colId} and row: ${rowId}`);
-                updateLrById(colId, rowId, inputValue);
-            }
-            console.log('Value saved:', inputValue);
+        if (inputValue !== initialValue) {
+            console.log(`Wow there is changes in col: ${colId} and row: ${rowId}`);
+            updateJevById(colId, rowId, inputValue);
         }
+        console.log('Value saved:', inputValue);
     };
 
     return (
