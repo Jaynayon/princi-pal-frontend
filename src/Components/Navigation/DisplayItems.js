4e449ed531e640574974698fb9cb3ffc146ccdf0
Jaynayon <jay.nayonjr@cit.edu>
2024-10-21 14:39:09 +0800

    Optimize and relocate ProfileTab function

--- src/Components/Navigation/DisplayItems.js
+++ src/Components/Navigation/DisplayItems.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { memo, useState } from 'react';
 
 // Material-UI imports
 import { Link } from 'react-router-dom';
@@ -9,32 +9,17 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 
 // Material-UI icons imports
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import PeopleIcon from '@mui/icons-material/People';
 import LogoutIcon from '@mui/icons-material/Logout';
 import SettingsIcon from '@mui/icons-material/Settings';
-import InputAdornment from '@mui/material/InputAdornment';
-import { Person as PersonIcon, Email as EmailIcon } from '@mui/icons-material';
 
 // Custom component import
 import DisplaySchools from './DisplaySchools';
-import { useNavigationContext } from '../../Context/NavigationProvider';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Stack from '@mui/material/Stack';
-import Avatar from '@mui/material/Avatar';
-import { Typography } from '@mui/material';
-import { useAppContext } from '../../Context/AppProvider';
 import LogoutDialog from '../Modal/LogoutDialog';
 
-export function DisplayItems() {
+const DisplayItems = memo(({ list, selected, setSelected }) => {
     const theme = useTheme();
-    const { list, selected, setSelected } = useNavigationContext();
     const [logoutDialogOpen, setLogoutDialogOpen] = useState(false); // State to manage logout dialog
 
     console.log("display items rendered");
@@ -90,12 +75,12 @@ export function DisplayItems() {
                         <ListItemButton
                             key={index}
                             component={Link}
-                            to={index < 4 ? `/${item.toLowerCase()}` : '#'} // Logout route has not yet been implemented
+                            to={index < 4 && `/${item.toLowerCase()}`} // Logout modal will handle the logout route
                             selected={selected === item}
                             value={item}
                             onClick={() => {
                                 if (index < 4) {
-                                    setSelected(item);
+                                    setSelected(selected !== item && item); // Only update if it's not the same value
                                 } else {
                                     setLogoutDialogOpen(true); // Open logout dialog on logout button click
                                 }
@@ -124,199 +109,7 @@ export function DisplayItems() {
             />
         </>
     );
-}
-
-export const ProfileTab = () => {
-    const theme = useTheme();
-    const [selected, setSelected] = useState(false);
-    const [dialogOpen, setDialogOpen] = useState(false); // State to manage dialog open/close
-    const { currentUser } = useAppContext();
-
-    if (!currentUser) {
-        return null
-    }
-
-    const handleDialogOpen = () => {
-        setDialogOpen(true);
-    };
-
-    const handleDialogClose = () => {
-        setDialogOpen(false);
-        setSelected(false)
-    };
-    const adjustSecondaryTypography = () => {
-        // Define a threshold length for email after which font size will be reduced
-        const thresholdLength = 10;
-
-        // Check if email length exceeds the threshold
-        if (currentUser.email.length > thresholdLength) {
-            return { color: theme.navStyle.color, fontSize: 10 }; // Adjust font size if email is too long
-        }
-
-        return { color: theme.navStyle.color, fontSize: 12 }; // Use default font size
-    };
-
-    return (
-        <React.Fragment>
-            <ListItemButton
-                sx={{
-                    ...theme.navStyle.button,
-                    padding: '5px',
-                }}
-                selected={selected}
-                onClick={() => {
-                    setSelected(!selected);
-                    handleDialogOpen();
-                }}
-            >
-
-                <ListItemIcon
-                    sx={{
-                        minWidth: '40px',
-                        width: '50px'
-                    }}
-                >
-
-                    <AccountCircleIcon
-                        sx={{
-                            color: currentUser.avatar,
-                            fontSize: '35px',
-                            width: '100%',
-                        }}
-                    />
-
-                </ListItemIcon>
-
-                <Typography
-                    component="div"
-                    style={{
-                        overflow: 'hidden',
-                        textOverflow: 'ellipsis',
-                        whiteSpace: 'nowrap',
-                        width: '100%', // Ensure it takes up the full width of the container
-                    }}
-                >
-                    <ListItemText
-                        primary={`${currentUser.fname} ${currentUser.lname}`}
-                        secondary={currentUser.email}
-                        primaryTypographyProps={{
-                            fontWeight: 'bold',
-                            color: theme.navStyle.color,
-                            style: {
-                                overflow: 'hidden',
-                                textOverflow: 'ellipsis',
-                                whiteSpace: 'nowrap',
-                            },
-                        }}
-                        secondaryTypographyProps={adjustSecondaryTypography()}
-                    />
-                </Typography>
-            </ListItemButton>
-            <Dialog open={dialogOpen} onClose={handleDialogClose}>
-                <DialogTitle>My Profile</DialogTitle>
-                <DialogContent>
-                    <Stack spacing={2} margin={2} direction="row" alignItems="center">
-                        <Avatar sx={{ bgcolor: currentUser.avatar, width: 90, height: 90, bottom: 160 }} alt="User Avatar"> </Avatar>
-                        <Stack spacing={2}>
-                            <TextField
-                                sx={{ width: '100%' }}
-                                disabled
-                                id="username-field"
-                                label="Username"
-                                defaultValue={currentUser.username}
-                                margin="normal"
-                                InputProps={{
-                                    startAdornment: (
-                                        <InputAdornment position="start">
-                                            <PersonIcon />
-                                        </InputAdornment>
-                                    ),
-                                }}
-                            />
-                            <TextField
-                                sx={{ width: '100%' }}
-                                disabled
-                                id="first-name-field"
-                                label="First Name"
-                                defaultValue={currentUser.fname + " "}
-                                margin="normal"
-                                InputProps={{
-                                    startAdornment: (
-                                        <InputAdornment position="start">
-                                            <PersonIcon />
-                                        </InputAdornment>
-                                    ),
-                                }}
-                            />
-                            <TextField
-                                sx={{ width: '100%' }}
-                                disabled
-                                id="middle-name-field"
-                                label="Middle Name"
-                                defaultValue={currentUser.mname + " "}
-                                margin="normal"
-                                InputProps={{
-                                    startAdornment: (
-                                        <InputAdornment position="start">
-                                            <PersonIcon />
-                                        </InputAdornment>
-                                    ),
-                                }}
-                            />
-                            <TextField
-                                sx={{ width: '100%' }}
-                                disabled
-                                id="last-name-field"
-                                label="Last Name"
-                                defaultValue={currentUser.lname}
-                                margin="normal"
-                                InputProps={{
-                                    startAdornment: (
-                                        <InputAdornment position="start">
-                                            <PersonIcon />
-                                        </InputAdornment>
-                                    ),
-                                }}
-                            />
-                            <TextField
-                                sx={{ width: '100%' }}
-                                disabled
-                                id="email-field"
-                                label="Email"
-                                defaultValue={currentUser.email}
-                                margin="normal"
-                                InputProps={{
-                                    startAdornment: (
-                                        <InputAdornment position="start">
-                                            <EmailIcon />
-                                        </InputAdornment>
-                                    ),
-                                }}
-                            />
-                            <TextField
-                                disabled
-                                id="role-field"
-                                label="Role"
-                                defaultValue={currentUser.position}
-                                margin="normal"
-                                InputProps={{
-                                    startAdornment: (
-                                        <InputAdornment position="start">
-                                            <PersonIcon />
-                                        </InputAdornment>
-                                    ),
-                                }}
-                            />
-                        </Stack>
-                    </Stack>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleDialogClose} style={{ position: 'absolute', top: '10px', right: '10px', background: 'none', border: 'none', color: '#757575', fontSize: '1.5rem', cursor: 'pointer' }}>×</Button>
-                </DialogActions>
-            </Dialog>
-        </React.Fragment>
-    );
-};
+});
 
 export function VerticalLine({ width, color = 'black' }) {
     return (
@@ -337,3 +130,5 @@ export function VerticalLine({ width, color = 'black' }) {
         </div>
     );
 }
+
+export default DisplayItems;
